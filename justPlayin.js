b9ea31f749b1a93d3ac8bd8848b5c29eda0a4e08
Jonathan Slack <slack6000@gmail.com>
2022-05-07 21:27:57 -0500

    second  save state

---
+++ justPlayin.js
@@ -0,0 +1,44 @@
+let player1 = 'rock'
+
+let player2 = 'paper'
+
+if (player1 === player2) {
+    console.log('It is a draw')
+} else if (player1 === "rock" && player2 === "scissors") {
+    console.log('Player 1 wins!')
+} else if (player1 === "rock" && player2 === "paper") {
+    console.log("Player 2 Wins.")
+} else if (player1 === "scissors" && player2 === "paper") {
+    console.log("Player 1 wins! ")
+} else if (player1 === "scissors" && player2 === "rock") {
+    console.log('Player 2 Wins!')
+} else if (player1 === 'paper' && player2 === "rock") {
+    console.log('Player 1 Wins!')
+} else {
+    console.log('Player 2 Wins!')
+}
+
+
+let temperature = 71 
+
+if (temperature < 55) {
+    console.log('Better get a jacket.')
+} else if (temperature <= 70) {
+    console.log('Its way too nice outside so wear what you want')
+} else {
+    console.log("Wear a tank top, its getting hot!")
+}
+
+let numsArr = [1, 2, 3, 4, 5]
+
+for (let i = 0; i < numsArr.length; i++) {
+    console.log(numsArr[i])
+}
+numsArr.push(6, 7, 8, 9, 10)
+
+console.log(numsArr)
+
+numsArr.splice(0, 1,'stupid', 'amazing')
+
+console.log(numsArr)
+
